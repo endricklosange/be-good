b75995b99c55ed84fdd5644a184d11ae265887e8
VanhoveHugo <vanhovehugo1@gmail.com>
2023-09-12 11:25:46 +0200

    Init Repos

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Event Listener</title>
+</head>
+<body>
+    <h1>PHP Test</h1>
+    <p>
+        <?= "Hello World!" ?>
+    </p>
+</body>
+</html>
