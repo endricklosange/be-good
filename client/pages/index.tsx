de1ce9c39df0358a5d09c87afa9a35fb4b8f905d
VanhoveHugo <vanhovehugo1@gmail.com>
2023-09-15 11:12:27 +0200

    Add Name & Mail$

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,16 @@
 import Table from '../components/table'
-import { useEffect, useState } from 'react'
+import { useEffect, useLayoutEffect, useState } from 'react'
 
+
+interface User {
+  displayName: string,
+  mail: string
+}
 
 export default function Home() {
 
   let [data, setData] = useState<any[]>([])
+  let [teams, setTeams] = useState<User>({displayName: '', mail: ''})
 
   useEffect(() => {
     fetch('/api/events')
@@ -12,18 +18,30 @@ export default function Home() {
       .then(data => setData(data))
   }, [])
 
+  useLayoutEffect(() => {
+    fetch('/api/teams')
+      .then(res => res.json())
+      .then(data => setTeams(data))
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-start p-24">
-      <div className='max-w-4xl w-full'>
-        <div className='flex justify-end w-full mb-10 '>
-          <button 
+      <div className='max-w-xl w-full'>
+        <div className='flex w-full justify-between items-center mb-10'>
+          <div className='flex flex-col'>
+            <h1 className='text-2xl font-bold'>{teams.displayName}</h1>
+            <h3 className='text-sm text-gray-500'>{teams.mail}</h3>
+          </div>
+          <div className='flex'>
+            <button 
             className='mr-4 text-[#b89a52] active:bg-[#fcecd9] font-light uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150'>
             Import
-          </button>
-          <button 
-            className='bg-[#b89a52] text-white active:bg-[#b99a72] font-light uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150'>
+            </button>
+            <button 
+            className='bg-[#b89a52] text-white active:bg-[#d3ba81] font-light uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150'>
             Export
-          </button> 
+            </button> 
+          </div>
         </div>
         <Table data={data} />
       </div>
